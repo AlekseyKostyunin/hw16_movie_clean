42f837f3f023520e9818a3fb7518754e6a9f49b9
AlekseyKostyunin <kostunin-ag@mail.ru>
2024-07-11 22:22:00 +0500

    Внедрен Dagger2.

--- app/src/main/java/com/alekseykostyunin/hw16_movie_clean/data/MovieInfoApi.kt
+++ app/src/main/java/com/alekseykostyunin/hw16_movie_clean/data/MovieInfoApi.kt
@@ -2,7 +2,6 @@ package com.alekseykostyunin.hw16_movie_clean.data
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -11,11 +10,11 @@ import retrofit2.http.Headers
 private const val BASE_URL = "https://api.kinopoisk.dev/"
 
 object Retrofit {
-    val moshi = Moshi.Builder()
+    private val moshi: Moshi = Moshi.Builder()
         .addLast(KotlinJsonAdapterFactory())
         .build()
 
-    val retrofit = Retrofit.Builder()
+    private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
@@ -30,5 +29,5 @@ interface MovieInfoApi {
     )
 
     @GET("v1.4/movie/random?token=VRY7JS6-4TB4C8P-MR49NYR-PDYB8KE&notNullFields=poster.url")
-    suspend fun getMovie() : MovieDto
+    suspend fun getMovie(): MovieDto
 }
